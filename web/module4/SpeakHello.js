f123bf6dc2862d4361980e98796d8a5ea83c9802
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-01-05 20:41:08 +0100

    w

---
+++ web/module4/SpeakHello.js
@@ -0,0 +1,10 @@
+
+(function(window) {
+	let speakWord = "Hello";
+	let helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
